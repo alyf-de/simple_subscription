a7d41e9b55b0345bf792e5125dab1ad897d681b5
barredterra <14891507+barredterra@users.noreply.github.com>
2022-02-10 11:05:02 +0100

    feat: calculate period for other frequencies

--- simple_subscription/simple_subscription/doctype/simple_subscription/simple_subscription.py
+++ simple_subscription/simple_subscription/doctype/simple_subscription/simple_subscription.py
@@ -1,6 +1,7 @@
 # Copyright (c) 2022, ALYF GmbH and contributors
 # For license information, please see license.txt
 from datetime import date, timedelta
+from typing import Tuple
 from dateutil.relativedelta import relativedelta
 
 import frappe
@@ -12,10 +13,13 @@ class SimpleSubscription(Document):
 		invoice = frappe.new_doc("Sales Invoice")
 		invoice.customer = self.customer
 		for row in self.items:
-			invoice.append("items", {
-				"item_code": row.item,
-				"quantity": row.qty,
-			})
+			invoice.append(
+				"items",
+				{
+					"item_code": row.item,
+					"quantity": row.qty,
+				},
+			)
 		invoice.taxes_and_charges = self.taxes_and_charges
 		invoice.from_date = from_date
 		invoice.to_date = to_date
@@ -27,12 +31,14 @@ class SimpleSubscription(Document):
 @frappe.whitelist()
 def create_invoice_for_last_period(subscription_name):
 	subscription = frappe.get_doc("Simple Subscription", subscription_name)
-	from_date, to_date = get_dates(subscription.frequency)
+	invoice_date = get_invoice_date(date.today(), subscription.frequency)
+	from_date, to_date = get_period(invoice_date, subscription.frequency)
 	subscription.create_invoice(from_date, to_date)
 
 
 def process_subscriptions(frequency):
-	from_date, to_date = get_dates(frequency)
+	invoice_date = get_invoice_date(date.today(), frequency)
+	from_date, to_date = get_period(invoice_date, frequency)
 	for subscription_name in frappe.get_all(
 		"Simple Subscription",
 		filters={"docstatus": 1, "frequency": frequency},
@@ -42,15 +48,39 @@ def process_subscriptions(frequency):
 		subscription.create_invoice(from_date, to_date)
 
 
-def get_dates(frequency: str):
-	months = {
+def get_period(invoice_date: date, frequency: str) -> Tuple[date, date]:
+	frequency_map = {
 		"Monthly": 1,
 		"Quarterly": 3,
 		"Halfyearly": 6,
 		"Yearly": 12,
 	}
-	first_day_of_month = date.today().replace(day=1)
+
+	first_day_of_month = invoice_date.replace(day=1)
 	last_day_of_period = first_day_of_month - timedelta(days=1)
-	first_day_of_period = first_day_of_month - relativedelta(months=months[frequency])
+	first_day_of_period = first_day_of_month - relativedelta(
+		months=frequency_map[frequency]
+	)
 
 	return first_day_of_period, last_day_of_period
+
+
+def get_invoice_date(from_date: date, frequency: str) -> date:
+	"""
+	Quarterly:
+			02.10.2021 -> 01.10.2021
+			05.01.2022 -> 01.01.2022
+	"""
+	invoice_month = {
+		"Monthly": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+		"Quarterly": [1, 1, 1, 4, 4, 4, 7, 7, 7, 10, 10, 10],
+		"Halfyearly": [1] * 6 + [7] * 6,
+		"Yearly": [1] * 12,
+	}
+
+	first_day_of_month = from_date.replace(day=1)
+	months_delta = (
+		first_day_of_month.month
+		- invoice_month[frequency][first_day_of_month.month - 1]
+	)
+	return first_day_of_month - relativedelta(months=months_delta)
