688bc26ec22c8aafefbfb1929358a7c3549fb9ef
barredterra <14891507+barredterra@users.noreply.github.com>
2022-02-10 13:40:23 +0100

    fix: button

--- simple_subscription/simple_subscription/doctype/simple_subscription/simple_subscription.js
+++ simple_subscription/simple_subscription/doctype/simple_subscription/simple_subscription.js
@@ -5,8 +5,9 @@ frappe.ui.form.on('Simple Subscription', {
 	refresh: function(frm) {
 		if (frm.doc.docstatus !== 1) return;
 
-		frm.add_custom_button(__("Create invoice for last month"), () => frappe.call({
-			method: "simple_subscription.simple_subscription.doctype.simple_subscription.simple_subscription.create_invoice_for_last_month",
+		const translated_frequency = __(frm.doc.frequency, null, "Frequency of Subscription");
+		frm.add_custom_button(__("Create last {0} invoice", [translated_frequency]), () => frappe.call({
+			method: "simple_subscription.simple_subscription.doctype.simple_subscription.simple_subscription.create_invoice_for_last_period",
 			args: {
 				subscription_name: frm.doc.name,
 			},
