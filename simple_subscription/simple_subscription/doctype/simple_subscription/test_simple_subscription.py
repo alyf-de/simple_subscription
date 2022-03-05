9b9480dfaf38b2112bb41bbe5d456eec0343a9fc
barredterra <14891507+barredterra@users.noreply.github.com>
2022-03-05 15:47:22 +0100

    refactor: assertEquals -> assertEqual

--- simple_subscription/simple_subscription/doctype/simple_subscription/test_simple_subscription.py
+++ simple_subscription/simple_subscription/doctype/simple_subscription/test_simple_subscription.py
@@ -17,42 +17,42 @@ class TestSimpleSubscription(unittest.TestCase):
 		from_date = date(2022, 11, 7)
 
 		invoice_date = get_first_day_of_period(from_date, Frequency.Monthly)
-		self.assertEquals(invoice_date, date(2022, 11, 1))
+		self.assertEqual(invoice_date, date(2022, 11, 1))
 
 		invoice_date = get_first_day_of_period(from_date, Frequency.Quarterly)
-		self.assertEquals(invoice_date, date(2022, 10, 1))
+		self.assertEqual(invoice_date, date(2022, 10, 1))
 
 		invoice_date = get_first_day_of_period(from_date, Frequency.Halfyearly)
-		self.assertEquals(invoice_date, date(2022, 7, 1))
+		self.assertEqual(invoice_date, date(2022, 7, 1))
 
 		invoice_date = get_first_day_of_period(from_date, Frequency.Yearly)
-		self.assertEquals(invoice_date, date(2022, 1, 1))
+		self.assertEqual(invoice_date, date(2022, 1, 1))
 
 	def test_get_period_start_and_end_dates(self):
 		invoice_date = date(2022, 2, 1)
 		start_date, end_date = get_period_start_and_end_dates(
 			invoice_date, Frequency.Monthly
 		)
-		self.assertEquals(start_date, date(2022, 1, 1))
-		self.assertEquals(end_date, date(2022, 1, 31))
+		self.assertEqual(start_date, date(2022, 1, 1))
+		self.assertEqual(end_date, date(2022, 1, 31))
 
 		invoice_date = date(2022, 1, 1)
 		start_date, end_date = get_period_start_and_end_dates(
 			invoice_date, Frequency.Quarterly
 		)
-		self.assertEquals(start_date, date(2021, 10, 1))
-		self.assertEquals(end_date, date(2021, 12, 31))
+		self.assertEqual(start_date, date(2021, 10, 1))
+		self.assertEqual(end_date, date(2021, 12, 31))
 
 		invoice_date = date(2022, 1, 1)
 		start_date, end_date = get_period_start_and_end_dates(
 			invoice_date, Frequency.Halfyearly
 		)
-		self.assertEquals(start_date, date(2021, 7, 1))
-		self.assertEquals(end_date, date(2021, 12, 31))
+		self.assertEqual(start_date, date(2021, 7, 1))
+		self.assertEqual(end_date, date(2021, 12, 31))
 
 		invoice_date = date(2022, 1, 1)
 		start_date, end_date = get_period_start_and_end_dates(
 			invoice_date, Frequency.Yearly
 		)
-		self.assertEquals(start_date, date(2021, 1, 1))
-		self.assertEquals(end_date, date(2021, 12, 31))
+		self.assertEqual(start_date, date(2021, 1, 1))
+		self.assertEqual(end_date, date(2021, 12, 31))
