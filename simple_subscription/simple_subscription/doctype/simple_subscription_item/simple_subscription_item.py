36bd74ab2b5fe2d84daa2266714e3b6fc09a57f9
HenningWendtland <156231187+HenningWendtland@users.noreply.github.com>
2024-09-12 18:44:12 +0200

    feat: display virtual field "current rate"

--- simple_subscription/simple_subscription/doctype/simple_subscription_item/simple_subscription_item.py
+++ simple_subscription/simple_subscription/doctype/simple_subscription_item/simple_subscription_item.py
@@ -1,8 +1,52 @@
 # Copyright (c) 2022, ALYF GmbH and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
+from erpnext.stock.get_item_details import get_item_details
+from erpnext.accounts.party import get_party_details
+from frappe.utils import today
+
 
 class SimpleSubscriptionItem(Document):
-	pass
+	@property
+	def current_rate(self):
+		parent = frappe.get_doc("Simple Subscription", self.parent)
+
+		party_details = get_party_details(
+			party=parent.customer,
+			account=None,
+			party_type="Customer",
+			company=parent.company,
+			posting_date=today(),
+			currency=parent.currency,
+			doctype="Sales Invoice",
+			fetch_payment_terms_template=False,
+		)
+
+		item_details = get_item_details(
+			{
+				"item_code": self.item,
+				"company": parent.company,
+				"doctype": "Sales Invoice",
+				"currency": parent.currency,
+				"price_list_currency": parent.currency,
+				"qty": self.qty,
+				"plc_conversion_rate": 1,
+				"conversion_rate": 1,
+				"customer": parent.customer,
+				"transaction_date": today(),
+				"price_list": party_details.selling_price_list,
+			}
+		)
+		return item_details.price_list_rate - (item_details.discount_amount or 0)
+
+	@property
+	def current_description(self):
+		item = frappe.get_doc("Item", self.item)
+		return item.description
+
+	@property
+	def item_name(self):
+		item = frappe.get_doc("Item", self.item)
+		return item.item_name
