32693d2c0ca2fc10db35b39b34740395e5e1d3dd
HenningWendtland <156231187+HenningWendtland@users.noreply.github.com>
2024-09-13 13:12:36 +0200

    chore: update tests

--- simple_subscription/simple_subscription/doctype/simple_subscription/test_simple_subscription.py
+++ simple_subscription/simple_subscription/doctype/simple_subscription/test_simple_subscription.py
@@ -6,23 +6,49 @@ import unittest
 from datetime import date
 
 from .simple_subscription import (
-	get_first_day_of_period,
+	get_calendar_period,
+	get_date_period,
 	Frequency,
 )
 
 
 class TestSimpleSubscription(unittest.TestCase):
-	def test_get_first_day_of_period(self):
-		from_date = date(2022, 11, 7)
+	def test_get_calendar_period(self):
+		eval_date = date(2022, 11, 7)
 
-		invoice_date = get_first_day_of_period(from_date, Frequency.Monthly)
-		self.assertEqual(invoice_date, date(2022, 11, 1))
+		from_date, to_date = get_calendar_period(eval_date, Frequency.Monthly)
+		self.assertEqual(from_date, date(2022, 11, 1))
+		self.assertEqual(to_date, date(2022, 11, 30))
 
-		invoice_date = get_first_day_of_period(from_date, Frequency.Quarterly)
-		self.assertEqual(invoice_date, date(2022, 10, 1))
+		from_date, to_date = get_calendar_period(eval_date, Frequency.Quarterly)
+		self.assertEqual(from_date, date(2022, 10, 1))
+		self.assertEqual(to_date, date(2022, 12, 31))
 
-		invoice_date = get_first_day_of_period(from_date, Frequency.Halfyearly)
-		self.assertEqual(invoice_date, date(2022, 7, 1))
+		from_date, to_date = get_calendar_period(eval_date, Frequency.Halfyearly)
+		self.assertEqual(from_date, date(2022, 7, 1))
+		self.assertEqual(to_date, date(2022, 12, 31))
 
-		invoice_date = get_first_day_of_period(from_date, Frequency.Yearly)
-		self.assertEqual(invoice_date, date(2022, 1, 1))
+		from_date, to_date = get_calendar_period(eval_date, Frequency.Yearly)
+		self.assertEqual(from_date, date(2022, 1, 1))
+		self.assertEqual(to_date, date(2022, 12, 31))
+
+def test_get_date_period(self):
+		eval_date = date(2022, 11, 7)
+		initial_date = date(2022, 6, 25)
+
+		from_date, to_date = get_date_period(eval_date, Frequency.Monthly, initial_date)
+		self.assertEqual(from_date, date(2022, 10, 25))
+		self.assertEqual(to_date, date(2022, 11, 24))
+
+		from_date, to_date = get_date_period(eval_date, Frequency.Quarterly, initial_date)
+		self.assertEqual(from_date, date(2022, 9, 25))
+		self.assertEqual(to_date, date(2022, 12, 24))
+
+		from_date, to_date = get_date_period(eval_date, Frequency.Halfyearly, initial_date)
+		self.assertEqual(from_date, date(2022, 6, 25))
+		self.assertEqual(to_date, date(2022, 12, 24))
+
+		from_date, to_date = get_date_period(eval_date, Frequency.Yearly, initial_date)
+		self.assertEqual(from_date, date(2022, 6, 25))
+		self.assertEqual(to_date, date(2023, 6, 24))
+
